d70c88b9d40bbabf3afb629f89467b52839a449b
Josh Price <josh@alembic.com.au>
2018-03-26 14:48:17 +1100

    Format mix file too

--- mix.exs
+++ mix.exs
@@ -2,24 +2,30 @@ defmodule Swagger.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :libswagger,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :libswagger,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger, :poison, :yaml_elixir, :ex_json_schema, :maxwell]]
+    [
+      applications: [:logger, :poison, :yaml_elixir, :ex_json_schema, :maxwell]
+    ]
   end
 
   defp deps do
-    [{:poison, "~> 3.0", override: true},
-     {:maxwell, github: "bitwalker/maxwell"},
-     {:hackney, "~> 1.6", optional: true},
-     {:yamerl, github: "yakaz/yamerl", tag: "v0.4.0", override: true},
-     {:yaml_elixir, "~> 1.1"},
-     {:ex_json_schema, "~> 0.5"}]
+    [
+      {:poison, "~> 3.0", override: true},
+      {:maxwell, github: "bitwalker/maxwell"},
+      {:hackney, "~> 1.6", optional: true},
+      {:yamerl, github: "yakaz/yamerl", tag: "v0.4.0", override: true},
+      {:yaml_elixir, "~> 1.1"},
+      {:ex_json_schema, "~> 0.5"}
+    ]
   end
 end
